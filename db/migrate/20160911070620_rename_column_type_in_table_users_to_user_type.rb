05085ecab06572a7737db62f83e09a93d0a0a8aa
Sid Tantia <sstantia@gmail.com>
2016-09-11 01:59:57 -0700

    heroku

--- db/migrate/20160911070620_rename_column_type_in_table_users_to_user_type.rb
+++ db/migrate/20160911070620_rename_column_type_in_table_users_to_user_type.rb
@@ -1,5 +1,5 @@
 class RenameColumnTypeInTableUsersToUserType < ActiveRecord::Migration
   def change
-    rename_column :users, :user_type, :user_type
+    rename_column :users, :type, :user_type
   end
 end
